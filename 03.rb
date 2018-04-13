d6a73c308c802865454adf54d0169134d59011d6
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2018-04-13 16:28:46 -0500

    AoC 2016: Day 3.1

--- 03.rb
+++ 03.rb
@@ -1,8 +1,12 @@
 require 'irb'
 
-triangles = File.readlines(ARGV[0]).select do |line|
-  sides = line.strip.split(' ').map(&:to_i).sort
-  sides[0] + sides[1] > sides[2]
+triangles = File.readlines(ARGV[0]).map do |line|
+  line.strip.split(' ').map(&:to_i)
+end.each_slice(3).map do |group|
+  group.transpose
+end.flatten(1).select do |triangle|
+  triangle.sort!
+  triangle[0] + triangle[1] < triangle[2]
 end
 
 puts triangles.size
