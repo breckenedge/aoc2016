e079f63500fcdc91ddfbb81c8cfa400d766263c9
Aaron Breckenridge <aaronbreckenridge@gmail.com>
2018-04-19 11:43:50 -0500

    AoC 2016: Day 5.1

--- 05.rb
+++ 05.rb
@@ -2,31 +2,38 @@ require 'pry'
 require 'digest'
 
 class PasswordCracker
-  attr_reader :input
+  attr_reader :cinematic, :input
 
-  def initialize(input)
+  def initialize(input, cinematic: false)
+    @cinematic = cinematic
     @input = input
   end
 
   def password
     index = 0
-    interestings = []
+    interestings = Array.new(8)
 
     loop do
       digest = Digest::MD5.hexdigest("#{input}#{index}")
 
-      if digest[0,5] == '00000'
-        interestings << digest[5]
+      if digest =~ /^00000[0-7]/
+        interestings[digest[5].to_i] ||= digest[6]
       end
 
       index += 1
 
-      break if interestings.size == 8
+      print "\r#{interestings.map.with_index { |v, i| v.nil? ? digest[i] : v }.join}" if cinematic
+
+      if interestings.none?(&:nil?)
+        print "\n" if cinematic
+        break
+      end
     end
     interestings.join
   end
 end
 
 if __FILE__ == $0
-  puts PasswordCracker.new(ARGV[0]).password
+  password = PasswordCracker.new(ARGV[0], cinematic: !ENV['CINEMATIC'].nil?).password
+  puts password
 end
